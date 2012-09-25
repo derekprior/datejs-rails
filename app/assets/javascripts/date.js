5ed938f21e100046ab72a7c1cb9face88c71e7d4
Charles Lowell <cowboyd@thefrontside.net>
2012-09-26 02:23:02 +0300

    change load order to satisfy parser

--- app/assets/javascripts/date.js
+++ app/assets/javascripts/date.js
@@ -1,8 +1,8 @@
 // This is the default set of what you get when you require date in
 // your application. Each individual library can be used separately
 //
+//= require date/en-US
 //= require date/core
 //= require date/parser
 //= require date/sugarpak
 //= require date/time
-//= require date/en-US
